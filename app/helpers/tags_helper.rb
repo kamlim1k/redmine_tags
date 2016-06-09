6874e050d9cf4d47e686888cf4d1ee31ced65651
Nikolai Komlichenko <nkomlichenko@gloriumtech.com>
2016-06-09 18:38:23 -0400

    change color based on perceptive luminance

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -14,28 +14,35 @@ module TagsHelper
   def render_tag_link(tag, options = {})
     filters = [[:tags, '=', tag.name]]
     filters << [:status_id, 'o'] if options[:open_only]
-    tag_style, link_style = if RedmineTags.settings[:issues_use_colors].to_i > 0
-      tag_color, link_color = tag_color tag
-      [{class: 'tag-label-color', 
-          style: "background-color: #{ tag_color }"},{style: "color: #{ link_color }"}]
-    else
-      [{class: 'tag-label'}, {}]
-    end
+    style, link_style =
+        if RedmineTags.settings[:issues_use_colors].to_i > 0
+          bg_color = tag_color tag
+          ln_color = link_color bg_color
+          [{class: 'tag-label-color', style: "background-color: #{ bg_color }"}, {style: "color: #{ ln_color }"}]
+        else
+          [{class: 'tag-label'}, {}]
+        end
     if options[:use_search]
-      content = link_to tag, { controller: 'search', action: 'index',
-        id: @project, q: tag.name, wiki_pages: true, issues: true }, link_style
+      content = link_to tag, {controller: 'search', action: 'index',
+                              id: @project, q: tag.name, wiki_pages: true, issues: true}, link_style
     else
-      content = link_to_filter tag.name, filters, project_id: @project, link_style
+      content = link_to_filter tag.name, filters, {project_id: @project}, link_style
     end
     if options[:show_count]
       content << content_tag('span', "(#{ tag.count })", class: 'tag-count')
     end
-    content_tag 'span', content, tag_style
+    content_tag 'span', content, style
   end
 
   def tag_color(tag)
     tag_name = tag.respond_to?(:name) ? tag.name : tag
     "##{ Digest::MD5.hexdigest(tag_name)[0..5] }"
+  end
+
+  def link_color(bg_color)
+    bg_color = bg_color.gsub('#', '')
+    rgb = bg_color.scan(/../).map { |color| color.hex }
+    (1 - (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255) < 0.5 ? '#000000' : '#FFFFFF'
   end
 
   # Renders list of tags
@@ -57,17 +64,17 @@ module TagsHelper
       tags = tags.to_a
       case sorting = "#{ RedmineTags.settings[:issues_sort_by] }:#{ RedmineTags.settings[:issues_sort_order] }"
         when 'name:asc'
-          tags.sort! {|a, b| a.name <=> b.name }
+          tags.sort! { |a, b| a.name <=> b.name }
         when 'name:desc'
-          tags.sort! {|a, b| b.name <=> a.name }
+          tags.sort! { |a, b| b.name <=> a.name }
         when 'count:asc'
-          tags.sort! {|a, b| a.count <=> b.count }
+          tags.sort! { |a, b| a.count <=> b.count }
         when 'count:desc'
-          tags.sort! {|a, b| b.count <=> a.count }
+          tags.sort! { |a, b| b.count <=> a.count }
         else
           # Unknown sorting option. Fallback to default one
           logger.warn "[redmine_tags] Unknown sorting option: <#{ sorting }>"
-          tags.sort! {|a, b| a.name <=> b.name }
+          tags.sort! { |a, b| a.name <=> b.name }
       end
       if :list == style
         list_el, item_el = 'ul', 'li'
@@ -82,13 +89,13 @@ module TagsHelper
       content = content.html_safe
       tag_cloud tags, (1..8).to_a do |tag, weight|
         content << ' '.html_safe <<
-          content_tag(item_el, render_tag_link(tag, options),
-            class: "tag-nube-#{ weight }",
-            style: (:simple_cloud == style ? 'font-size: 1em;' : '')) <<
-          ' '.html_safe
+            content_tag(item_el, render_tag_link(tag, options),
+                        class: "tag-nube-#{ weight }",
+                        style: (:simple_cloud == style ? 'font-size: 1em;' : '')) <<
+            ' '.html_safe
       end
       content_tag list_el, content, class: 'tags',
-        style: (:simple_cloud == style ? 'text-align: left;' : '')
+                  style: (:simple_cloud == style ? 'text-align: left;' : '')
     end
   end
 
